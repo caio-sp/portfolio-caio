80b77d283db33d795e25057311609bc0ca964092
Caio Rodrigues <caio.rodrigues.sp@gmail.com>
2023-11-06 16:41:12 -0300

    Criados os componentes principais em jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import Hero from './components/Hero'
+import About from './components/About'
+import Works from './components/Works'
+import Timeline from './components/Timeline'
+import Contact from './components/Contact'
+
 function App() {
 
   return (
     <div>
-      Hello world!
+      <Hero />
+      <About />
+      <Works />
+      <Timeline />
+      <Contact />
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Hero = () => {
-    return{
+    return (
         <div>Hero</div>
-    }
+    )
 }
 
 export default Hero
