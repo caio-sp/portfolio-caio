c71f8d6356ca7a4a46d898732dcd3b42aabcb5b1
Caio Rodrigues <caio.rodrigues.sp@gmail.com>
2023-11-09 12:36:45 -0300

    added styeld-components

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Works from './components/Works'
 import Timeline from './components/Timeline'
-import Contact from './components/Contact'
+import Works from './components/Works'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
 
 function App() {
 
   return (
-    <div>
+    <Container>
       <Hero />
       <About />
       <Works />
       <Timeline />
       <Contact />
-    </div>
+    </Container>
   )
 }
 
