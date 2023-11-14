5a934837664d80ebddd71d6c84bda98f21c705f6
Caio Rodrigues <caio.rodrigues.sp@gmail.com>
2023-11-14 16:10:20 -0300

    novos componentes criados

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import {  Nav, NavDropdown, Offcanvas,Container, Navbar } from 'react-bootstrap'
+import {  Nav, Offcanvas, Container, Navbar } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -21,9 +21,11 @@ const Navbar_ = () => {
   return (
    <>
     {[false].map((expand) => (
-      <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+      <Navbar key={expand} expand={expand} className="bg-dark mb-3">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand href="#">
+            <Logo src="./img/logoCaio.png" className="d-inline-block align-top px-2"  />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
@@ -39,19 +41,6 @@ const Navbar_ = () => {
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="#action1">Home</Nav.Link>
                 <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown
-                  title="Dropdown"
-                  id={`offcanvasNavbarDropdown-expand-${expand}`}
-                >
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
               </Nav>
               
                 
